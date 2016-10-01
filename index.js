e35a53e9e2a000a565511be7b2278d0f7850abba
raulnabarret <raulnabarret@gmail.com>
2016-10-01 18:00:25 -0500

    Add dependencies: Body Parser and Nodemon

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 'use strict'
 
 var express = require('express')
+var bodyParser = require('body-parser')
 
 var app = express()
+var port = process.env.PORT || 3000 
 
-app.listen(3000, () => {
-	console.log('Running on port: 3000')
+app.use(bodyParser.urlencoded({ extended:false }))
+app.use(bodyParser.json())
+
+app.listen(port, () => {
+	console.log(`Running on port: ${port}`)
 })
