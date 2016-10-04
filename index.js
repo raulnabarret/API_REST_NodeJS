653d5ebd0f2814933c24c33afefe0858cf2066d0
raulnabarret <raulnabarret@gmail.com>
2016-10-03 22:34:33 -0500

    Add endpoints GET, POST, PUT, DELETE

--- index.js
+++ index.js
@@ -9,10 +9,27 @@ var port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({ extended:false }))
 app.use(bodyParser.json())
 
-app.get('/hola', function (req, res) {
-	res.send({
-		message: 'Hola Mundo!',
-	})
+app.get('/api/product', (req,res) => {
+	res.send(200, { 'products': [] })
+})
+
+app.get('/api/product/:productId', (req,res) => {
+	
+})
+
+app.post('/api/product', (req, res) => {
+
+	console.log(req.body)
+	res.status(200).send({message: 'El producto se ha agregado'})
+
+})
+
+app.put('/api/product/:productId', (req, res) => {
+
+})
+
+app.delete('/api/product/:productId', (req, res) => {
+
 })
 
 app.listen(port, () => {
