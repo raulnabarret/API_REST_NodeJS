11583d4972a32fff44ebdfeef63f371c936aa11f
raulnabarret <raulnabarret@gmail.com>
2016-10-04 22:05:00 -0500

    Add mongoose

--- index.js
+++ index.js
@@ -2,25 +2,26 @@
 
 var express = require('express')
 var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
 
 var app = express()
-var port = process.env.PORT || 3000 
+var port = process.env.PORT || 3000
 
-app.use(bodyParser.urlencoded({ extended:false }))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.get('/api/product', (req,res) => {
-	res.send(200, { 'products': [] })
+app.get('/api/product', (req, res) => {
+    res.send(200, { 'products': [] })
 })
 
-app.get('/api/product/:productId', (req,res) => {
-	
+app.get('/api/product/:productId', (req, res) => {
+
 })
 
 app.post('/api/product', (req, res) => {
 
-	console.log(req.body)
-	res.status(200).send({message: 'El producto se ha agregado'})
+    console.log(req.body)
+    res.status(200).send({ message: 'El producto se ha agregado' })
 
 })
 
@@ -32,6 +33,13 @@ app.delete('/api/product/:productId', (req, res) => {
 
 })
 
-app.listen(port, () => {
-	console.log(`Running on port: ${port}`)
+mongoose.connect('mongodb://localhost:27017/shop', (err, res) => {
+    if (err) return console.log(err)
+
+    console.log('Database connected')
+
+    app.listen(port, () => {
+        console.log(`Running on port: ${port}`)
+    })
+
 })
